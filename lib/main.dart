c0b164efa01c786f3776dc8b9ca84b295f1c0f6b
15680631503 <qq904645987@163.com>
2020-06-06 20:17:02 +0800

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,35 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 //创建main函数
 void main() {
-  runApp() => MyApp();
+  runApp(MyApp());
 }
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-        title: 'Hello world',
-        home: Scaffold(
-            appBar: AppBar(
-                title: Center(
-                    child:Text('我是标题')
-                )
-            ),
-            body: Center(
-              child: ContentWidget(),
-            )
-        )
+      title: 'hdjs',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("chenss",textDirection: TextDirection.ltr,)
+        ),
+        body: Text("hello",textDirection: TextDirection.ltr,)
+      )
     );
-  }
-}
-class ContentWidget extends StatelessWidget{
-  @override
-  Widget build(BuildContext context){
-    return Text(
-      'Hello world',
-      textDirection: TextDirection.ltr,
-      style: TextStyle(
-          fontSize: 30,
-          color: Colors.amberAccent
-      ));
   }
 }
