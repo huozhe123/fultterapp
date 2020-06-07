bf8908eff2e3c9f4022d60e74b927ae54c0567ff
15680631503 <qq904645987@163.com>
2020-06-07 20:30:07 +0800

    '计数器案例实现'

--- lib/main.dart
+++ lib/main.dart
@@ -9,51 +9,80 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Hello world",
       home: Scaffold(
         appBar: AppBar(
           title: Text("chenss"),
         ),
-        body: HomeContent(),
+        body: ContentWidget(),
       )
     );
   }
 }
-class HomeContent extends StatelessWidget{
+
+class ContentWidget extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return ContentWidgetState();
+
+  }
+}
+class ContentWidgetState extends State<ContentWidget>{
+  int counter = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(10),
-      child: ListView( //ListView包裹超出部分隐藏，可滚动显示
-        children: <Widget>[
-          ProductItem("Apple1","macbook1","https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/bd_logo1_31bdc765.png"),
-          ProductItem("Apple2","macbook2","https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/bd_logo1_31bdc765.png"),
-          ProductItem("Apple3","macbook3","https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/bd_logo1_31bdc765.png"),
-        ],
-      )
-    );
-  }
-}
-class ProductItem extends StatelessWidget{
-  final String title;
-  final String subTitle;
-  final String imageUrl;
-  ProductItem(this.title,this.subTitle,this.imageUrl);//简写形式
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(20.0),
-      decoration: BoxDecoration(
-        border: Border.all(width: 3)
-      ),
+    return Center(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Text(title,style: TextStyle(fontSize: 20),),
-          Text(subTitle,style: TextStyle(fontSize: 18),),
-          SizedBox(height: 10,),//间距
-          Image.network(imageUrl)
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                  onPressed:(){
+                    print("监听到按钮点击！！");
+                    setState(() {
+                      counter--;
+                    });
+                  },
+                  child:Icon(Icons.close)
+              ),
+              RaisedButton(
+                  onPressed:(){
+                    print("监听到按钮点击！！");
+                    setState(() {
+                      counter++;
+                    });
+                  },
+                  child:Icon(Icons.add)
+              )
+            ],
+          ),
+          Text("当前计数：$counter",style:TextStyle(fontSize: 25)),
         ],
       ),
     );
   }
 }
+//不能实现
+//class ContextWidget extends StatelessWidget{
+//  final int counter = 0;
+//  @override
+//  Widget build(BuildContext context) {
+//    return Center(
+//      child: Column(
+//        mainAxisAlignment: MainAxisAlignment.center,
+//        children: <Widget>[
+//          RaisedButton(
+//              onPressed:(){
+//                print("监听到按钮点击！！");
+//              },
+//              child:Text('计数+1')
+//          ),
+//          Text("当前计数：$counter",style:TextStyle(fontSize: 25)),
+//        ],
+//      ),
+//    );
+//  }
+//}
