84ce88fae99c9029ede13432b1b11c2c97968a0d
15680631503 <qq904645987@163.com>
2020-06-07 16:12:41 +0800

    '阶段案例实现

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,55 @@ void main() {
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget{
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'hdjs',
       home: Scaffold(
         appBar: AppBar(
-          title: Text("chenss",textDirection: TextDirection.ltr,)
+          title: Text("chenss"),
         ),
-        body: Text("hello",textDirection: TextDirection.ltr,)
+        body: HomeContent(),
       )
+    );
+  }
+}
+class HomeContent extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: ListView( //ListView包裹超出部分隐藏，可滚动显示
+        children: <Widget>[
+          ProductItem("Apple1","macbook1","https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/bd_logo1_31bdc765.png"),
+          ProductItem("Apple2","macbook2","https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/bd_logo1_31bdc765.png"),
+          ProductItem("Apple3","macbook3","https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/bd_logo1_31bdc765.png"),
+        ],
+      )
+    );
+  }
+}
+class ProductItem extends StatelessWidget{
+  final String title;
+  final String subTitle;
+  final String imageUrl;
+  ProductItem(this.title,this.subTitle,this.imageUrl);//简写形式
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20.0),
+      decoration: BoxDecoration(
+        border: Border.all(width: 3)
+      ),
+      child: Column(
+        children: <Widget>[
+          Text(title,style: TextStyle(fontSize: 20),),
+          Text(subTitle,style: TextStyle(fontSize: 18),),
+          SizedBox(height: 10,),//间距
+          Image.network(imageUrl)
+        ],
+      ),
     );
   }
 }
